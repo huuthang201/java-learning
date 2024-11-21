f6e1a55678b90248dce79a8da9caa17be9829a86
huuthang201 <thangnh2001@gmail.com>
2024-11-22 00:49:47 +0700

    condition

---
+++ src/main/java/org/example/ifelse.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class ifelse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter your electricity bill amount: ");
+        int choice = scanner.nextInt();
+        System.out.println("enter the length of the array: ");
+        int length = scanner.nextInt();
+        int[] arr = new int[length];
+        System.out.println("enter the elements of the array: ");
+        for (int i = 0; i < length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("check choice in the array: ");
+        boolean found = false;
+        for (int i = 0; i < length; i++) {
+            if (arr[i] == choice) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            System.out.println("choice is in the array");
+        } else {
+            System.out.println("choice is not in the array");
+        }
+
+    }
+}
