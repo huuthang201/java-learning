8b740ef21ed6cd27deba62d8ffceb0305291b013
huuthang201 <thangnh2001@gmail.com>
2024-11-22 12:56:13 +0700

    Rename filename

--- src/main/java/org/example/Condition.java
+++ src/main/java/org/example/Condition.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-public class ifelse {
+public class Condition {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter your electricity bill amount: ");
