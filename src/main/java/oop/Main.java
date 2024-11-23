05c18721d669886da3cdb39150e1d063da978918
huuthang201 <thangnh2001@gmail.com>
2024-11-23 22:28:55 +0700

    oop

---
+++ src/main/java/oop/Main.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class Main {
+    public static void main(String[] args) {
+        Student bepiu = new Student();
+        bepiu.setName("Bep iu");
+        bepiu.setAge(20);
+
+        System.out.println("Name: " + bepiu.getName());
+        System.out.println("Age: " + bepiu.getAge());
+    }
+}
